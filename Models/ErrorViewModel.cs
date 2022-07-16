7f4eed848848e49202fbabc47e318a7403dd1d1a
SashaMaksyutenko <84481245+SashaMaksyutenko@users.noreply.github.com>
2022-07-16 20:43:31 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace bookService.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
