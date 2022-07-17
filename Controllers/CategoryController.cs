71a270cefe72afd369ec6bea3cec402445d8315a
SashaMaksyutenko <84481245+SashaMaksyutenko@users.noreply.github.com>
2022-07-18 00:14:26 +0300

    "Category" and DB connection
    Creation of page Category and connection with SQL Server DB and making a style for pages.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bookService.Data;
+using bookService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace bookService.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Category>objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
+
